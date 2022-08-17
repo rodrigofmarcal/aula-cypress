523dcbec76e419e0dc32fcd10a09dc5e129851ae
Rodrigo Marcal <rodrigofmarcal@yahoo.com.br>
2022-08-17 19:28:58 -0300

    commit init

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,12 @@
+///<reference types="cypress" />
+
+context('funcionalidade login',()=>{
+
+    it('Deve fazer login com sucesso',()=>{
+
+    })
+
+    it('Deve exibir uma mensagem de erro ao inserir usuário ou senha inválidos ',()=>{
+
+    })
+})
